91965b9e0c354c8ffb2aa44a66b26f15d5eaa00d
hcetin03 <hcetin@stud.hs-bremen.de>
2026-06-09 17:46:48 +0200

    Wasserseite Layout korrigiert und CSS angepasst

--- src/app/wasser/wasser.ts
+++ src/app/wasser/wasser.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-wasser',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './wasser.html',
   styleUrl: './wasser.css',
 })
