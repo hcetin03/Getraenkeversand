57871f86261a7405d53bb38f3c993ab8cbb10eca
TiEgo7 <tego001@stud.hs-bremen.de>
2026-07-18 15:44:44 +0200

    rechnung und lagerbestand vervollständigt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcryptjs'); // Für die sichere Passwort-Verschlüsselung
+const PDFDocument = require('pdfkit');
 
 const app = express();
 
